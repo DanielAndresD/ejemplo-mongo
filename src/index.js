d74d5d4c0c25786cd3e5bc3bb9d6fa4fb449d819
Daniel <danielandresd998@gmail.com>
2022-12-29 01:08:25 -0500

    crear metodo post

--- src/index.js
+++ src/index.js
@@ -5,12 +5,15 @@ require("dotenv").config();
 
 //configuraciones
 const app = express();
+app.use(express.json()); //Serializa las entradas
 const port = process.env.PORT || 9000;
-
+const path=require("path");
 //Importacion de rutas
 const userRoutes = require("./routes/user");
 //enrutamiento del paquete routes
+
 app.use("/api", userRoutes);
+
 
 //mensaje de bienvenida para validar que se den respuestas en el servicio
 // routes
