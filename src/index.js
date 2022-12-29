97962b832cef3210a732e16b87e70ec820df22e2
Daniel <danielandresd998@gmail.com>
2022-12-29 08:59:03 -0500

    un mensaje

---
+++ src/index.js
@@ -0,0 +1,32 @@
+const express =require('express');
+const mongoose= require('mongoose');
+
+
+require("dotenv").config();
+//configuraciones
+const app=express();
+app.use(express.json()); //Serializa las entradas
+const port=process.env.PORT ||9000;
+const path=require("path");
+
+
+//Importacion de rutas
+const userRoutes = require("./routes/users");
+//enrutamiento del paquete routes
+app.use("/api", userRoutes);
+
+
+//validar operacion en el puerto
+app.listen(port,()=> console.log("El servicio opera correctamente en el puerto: ",port));
+
+
+//conexion a mongoDB
+mongoose
+  .connect(process.env.MONGO_DB_URI)
+  .then(() => console.log("Conectado a MongoDB"))
+  .catch((error) => console.error(error));
+//mensaje de bienvenida para validar que se den respuestas en el servicio
+// routes
+app.get('/',(req,res) =>{
+    res.send("Welcome to the jungle");
+});
